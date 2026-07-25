4fcd0786965410fd812d57be107314920c2c5aca
Swetha0407 <292122072+Swetha0407@users.noreply.github.com>
2026-07-25 10:44:46 +0530

    quick_sort

---
+++ quick_sort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int partition(int arr[],int l,int h){
+    int pivot=arr[h],i=l-1;
+    for(int j=l;j<h;j++){
+        if(arr[j]<pivot){
+            i++;
+            swap(arr[i],arr[j]);
+        }
+    }
+    swap(arr[i+1],arr[h]);
+    return i+1;
+}
+void quickSort(int arr[],int l,int h){
+    if(l<h){
+        int p=partition(arr,l,h);
+        quickSort(arr,l,p-1);
+        quickSort(arr,p+1,h);
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    quickSort(arr,0,n-1);
+    cout<<"Sorted array: ";
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<" ";
+    return 0;
+}
