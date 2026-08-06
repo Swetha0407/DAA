bdaa279d38437153adc0da45291d4f19fa5942fd
Swetha0407 <292122072+Swetha0407@users.noreply.github.com>
2026-08-06 21:24:59 +0530

    factorial using dp

---
+++ factorial.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    long long dp[n+1];
+    dp[0]=1;
+    for(int i=1;i<=n;i++)
+        dp[i]=dp[i-1]*i;
+    cout<<"Factorial: "<<dp[n];
+    return 0;
+}
